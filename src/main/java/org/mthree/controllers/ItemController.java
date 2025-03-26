544d1008a4f391c19f2d82bdb29f15733581b890
vivianc125 <vchenship@gmail.com>
2025-03-26 05:41:33 -1000

    Remove logging for ItemController

--- src/main/java/org/mthree/controllers/ItemController.java
+++ src/main/java/org/mthree/controllers/ItemController.java
@@ -52,24 +52,17 @@ public class ItemController {
             String itemId = itemResponse.getItem().getItemId();
             String institutionId = itemResponse.getItem().getInstitutionId();
 
-            System.out.println("accessToken = " + accessToken);
-            System.out.println("itemId = " + itemId);
-            System.out.println("institutionId = " + institutionId);
-
             Map<String, String> response = new HashMap<>();
             response.put("access_token", accessToken);
             response.put("item_id", itemId);
             response.put("institution_id", institutionId);
-
 
             Item item = new Item();
             item.setUserId(1L);
             item.setPlaidAccessToken(accessToken);
             item.setPlaidItemId(itemId);
 
-            System.out.println("Adding to item");
             itemService.addPlaidItem(item);
-            System.out.println("Added plaid item");
             return ResponseEntity.ok(response);
 
         } catch (IOException e) {
