aa704d23eb967434895586f98167dc260797998b
lyha1004 <lythituyetha@gmail.com>
2025-03-26 09:26:46 -0500

    adding user service impl

---
+++ src/main/java/org/mthree/service/UserServiceImpl.java
@@ -0,0 +1,51 @@
+import org.mthree.dao.UserDao;
+import org.mthree.dto.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Service
+public class UserServiceImpl {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final UserDao userDao;
+
+    public UserServiceImpl(JdbcTemplate jdbcTemplate, UserDao userDao) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.userDao = userDao;
+    }
+
+
+    public User createUser(String username, String password) {
+        String currentTime = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+        int userId = userDao.saveUser(username, password);  // Save user and password
+        return new User(userId, username, password, currentTime, currentTime);  // Return user with generated userId
+    }
+
+    public User checkUserCredentials(String username, String password) {
+        User user = userDao.getUserByUsername(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return user; // Return user if username and password match
+        }
+        return null;
+    }
+
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    public User getUserById(int id) {
+        return userDao.getUserById(id);
+    }
+
+    public int deleteUserById(int id) {
+        return userDao.deleteUserById(id);
+    }
+
+    public User getUserByUsername(String username) {
+        return userDao.getUserByUsername(username);
+    }
