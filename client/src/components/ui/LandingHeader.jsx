a68ce03fec52f38206fdc1b74b5f4e8534ceacb6
vivianc125 <vchenship@gmail.com>
2025-03-27 11:48:38 -1000

    Home Page redirect

--- client/src/components/ui/LandingHeader.jsx
+++ client/src/components/ui/LandingHeader.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Button } from "./button";
+import { Link } from 'react-router-dom';
 
 const LandingHeader = ({ title = "FlowTrack", onLoginClick, buttonText = "Log In", bgColor = "bg-white" }) => {
     return (
         <header className="w-full h-[88px] ${bgColor}">
             <div className="relative h-full flex items-center justify-between px-8">
                 {/* Logo */}
-                <div className="w-[70px] h-8 flex items-center">
+                <Link to="/" className="w-[70px] h-8 flex items-center">
                     <div className="[font-family:'Lato',Helvetica] font-bold text-2xl tracking-[0.20px] leading-8">
                         {title}
                     </div>
-                </div>
+                </Link>
+
 
                 {/* Login Button */}
                 <Button
